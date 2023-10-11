45216179ec0f301ff66907048ab1c21f93cc3ce9
Jewels <greengates4j@gmail.com>
2023-10-11 21:29:52 +0200

    first commit

---
+++ Mobile View/FrontPage/frontpage-script.js
@@ -0,0 +1,20 @@
+
+
+//function mapShow() {
+//    let mapElement = document.querySelectorAll("map-popup").style.display= "block";
+//    alert(mapElement);
+//}
+
+// .style.display= "block" 
+
+function mapShow() {
+  let ox = document.querySelector(".map-popup").style.display = "block"
+}
+
+  
+
+  
+function mapHide() {
+  let xo = document.querySelector(".map-popup").style.display = "none"
+}
+
